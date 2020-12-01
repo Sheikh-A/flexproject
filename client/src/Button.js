b20497ea73edf022220a903707747406ba457c4a
Ali Sheikh <ali.sheikh@flexport.com>
2020-11-30 23:37:43 -0800

    adding new formats

--- client/src/Button.js
+++ client/src/Button.js
@@ -2,19 +2,20 @@ import styled from "styled-components";
 const Button = styled.button`
   margin: 5px;
   font-weight: bold;
-  font-size: 1.2rem;
+  font-size: 1.1rem;
   margin-top: 10px;
   margin-bottom: 20px;
   border-radius: 8px;
   padding: 15px 20px 10px 10px;
   height: 50px;
-  width: 170px;
+  width: 165px;
+  background: white;
   border: none;
   border-radius: 3px;
-  color: white;
-  ${props => (props.type === "primary" ? `background: #2196F3` : null)}
-  ${props => (props.type === "success" ? `background: #4CAF50` : null)}
-  ${props => (props.type === "danger" ? `background: #F44336` : null)}
-  ${props => (props.type === "warning" ? `background: #502394` : null)}
+  color: black;
+  ${props => (props.type === "primary" ? `border: 3px solid #2196F3` : null)}
+  ${props => (props.type === "success" ? `border: 3px solid #4CAF50` : null)}
+  ${props => (props.type === "danger" ? `border: 3px solid #F44336` : null)}
+  ${props => (props.type === "warning" ? `border: 3px solid #502394` : null)}
 `;
 export default Button;
